46f6697b22891985d644a1f61ba1cb671b95a661
Kybbe <jacob.klaren.te16d@gmail.com>
2017-12-01 10:07:29 +0100

    Skapade Basic Html Sida med gammal register css kod enligt uppgift 4

---
+++ Uppgifter/Login/login.php
@@ -0,0 +1,17 @@
+<html>
+
+    <head>
+    
+        <title>Login</title>
+        <link href="Register.css" type="text/css" rel="stylesheet">
+        
+    </head>
+    
+    
+    <body>
+    
+        <h1>suck my aaaaaaaaassssssssssssss</h1>
+    
+    </body>
+
+</html>
