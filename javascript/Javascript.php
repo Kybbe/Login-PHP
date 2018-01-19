a9cad4d2649dd64b5c40106aa7d85c5a7de05620
Kybbe <jacob.klaren.te16d@gmail.com>
2018-01-19 10:44:53 +0100

    Upp JS:2
    Lade till Vacker  bild och gjorde den zoom:bar

--- javascript/Javascript.php
+++ javascript/Javascript.php
@@ -5,12 +5,24 @@
 <html>
     
     <body>
-            <button onClick="Meddelande()">Click Me</button>
+            <button id="Alert" onClick="Meddelande()">Click Me</button>
+        
+            <img src="emon.jpg" id="Hover" width="200px" height="300px" onmouseover="Expand(this)" onmouseout="Minimize(this)">
         
             <script>    
                 
                 function Meddelande() {
                     alert("DU SUGER BAJS")
+                }
+                
+                function Expand(x){
+                    x.style.height = "600px";
+                    x.style.width = "500px";
+                }
+                
+                function Minimize(x) {
+                    x.style.height = "300px";
+                    x.style.width = "200px";
                 }
                 
             </script>
