d59830f9c2a54eb0d13221ffab5ad5f0a59049b4
Kybbe <jacob.klaren.te16d@gmail.com>
2018-01-20 13:59:57 +0100

    JS:4,5 OCH css separation för funktionalitet

--- javascript/Javascript.php
+++ javascript/Javascript.php
@@ -1,23 +1,43 @@
-<head>
-
-</head>
-
 <html>
     
-    <style>
-    
-        #Sneaky {display: none};
-    
-    </style>
+    <head>
+        
+        <link href="Javascript.css" rel="stylesheet" type="text/css">
+        
+    </head>
     
     <body>
+        
+        <div id="Hideable First">
+            <div id="AlertDiv" >
+            <h2>Alert</h2>
             <button id="Alert" onClick="Meddelande()">Click Me</button>
+            </div>
         
+            <div id="HoverDiv">
+            <h2>Hover</h2>
             <img src="emon.jpg" id="Hover" width="200px" height="300px" onmouseover="Expand(this)" onmouseout="Minimize(this)">
+            </div>
         
+            <div id="HideDiv">
+            <h2>Hide Img</h2>
             <button id="HideImg" onclick="HideShowImg()">Vill du se en fin bild? KLICKA</button>
         
             <img id="Sneaky" src="emon.jpg" width="200px" height="300px">
+            </div>
+            
+            <div id="BGCDiv">
+            <h2>BackgroundColor</h2>
+            <button id="BakgrundsFärg" onclick="BakgrundsfärgChange()">Klicka så ska jag visa dig vägen // bästa färgen</button>
+            </div>
+        
+        </div>
+            
+            <div id="HT">
+            <h2>Secret message</h2>
+            <button id="HideAndText" onclick="HideAndTextFunc()">Göm allt och Display:a secret message!</button>
+            <p id="HideText">Baob e fin</p>
+            </div>
         
             <script>    
                 
@@ -41,6 +61,24 @@
                      x.style.display = "block"
                     }
                     else {x.style.display = "none"}
+                }
+                
+                function BakgrundsfärgChange() {
+                    r = Math.floor(Math.random()*255);
+                    g = Math.floor(Math.random()*255);
+                    b = Math.floor(Math.random()*255);
+                document.body.style.backgroundColor = "rgb("+r+","+g+","+b+")";
+                }
+                
+                function HideAndTextFunc() {
+                    var x = document.getElementById("HideText");
+                    var x2 = document.getElementById("Hideable First")
+                    if(x.style.display === "none") {
+                     x.style.display = "block";
+                     x2.style.display = "none";
+                    }
+                    else {x.style.display = "none";
+                         x2.style.display = "block";}
                 }
                 
             </script>
